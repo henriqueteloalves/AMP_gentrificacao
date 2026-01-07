d4ac3e81460356fc493c4516d662e91cb7fa7b31
Henrique Alves <henriquemanuel@gmail.com>
2026-01-07 16:16:29 +0000

    Initial commit: Leaflet map for Delphi participatory mapping

---
+++ js/main.js
@@ -0,0 +1,66 @@
+console.log(secoesAMP);
+// Criar o mapa
+var map = L.map('map').setView([41.15, -8.61], 11);
+
+// Base map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap'
+}).addTo(map);
+
+// Função de estilo
+function estilo(feature) {
+  return {
+    fillColor:
+      feature.properties.classificacao === 1 ? '#e41a1c' :
+      feature.properties.classificacao === 0 ? '#4daf4a' :
+      '#cccccc',
+    weight: 1,
+    color: '#333',
+    fillOpacity: 0.6
+  };
+}
+
+// Interação
+function emCadaFeature(feature, layer) {
+
+  layer.on('click', function () {
+
+    var resposta = prompt(
+      "Classificação:\n" +
+      "1 = Gentrificada\n" +
+      "0 = Não gentrificada"
+    );
+
+    if (resposta === '1' || resposta === '0') {
+      feature.properties.classificacao = parseInt(resposta);
+      layer.setStyle(estilo(feature));
+    }
+  });
+}
+
+// Carregar o GeoJSON
+L.geoJSON(secoesAMP, {
+  style: estilo,
+  onEachFeature: emCadaFeature
+}).addTo(map);
+
+// Add this at the bottom of main.js
+var exportButton = L.control({position: 'topright'});
+exportButton.onAdd = function(map){
+    var div = L.DomUtil.create('div', 'export-button');
+    div.innerHTML = '<button onclick="exportData()">Export Results</button>';
+    return div;
+};
+exportButton.addTo(map);
+
+function exportData(){
+    var geojsonData = JSON.stringify(secoesAMP); // your variable
+    var blob = new Blob([geojsonData], {type: "application/json"});
+    var url = URL.createObjectURL(blob);
+    var a = document.createElement('a');
+    a.href = url;
+    a.download = 'classified_results.json';
+    a.click();
+}
+
+
