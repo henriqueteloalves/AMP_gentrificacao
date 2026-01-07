befabaed69651f2d9107a862fc9a85649453f7cf
Henrique Alves <henriquemanuel@gmail.com>
2026-01-07 16:43:56 +0000

    Add export responses button for participants

--- js/main.js
+++ js/main.js
@@ -62,5 +62,33 @@ function exportData(){
     a.download = 'classified_results.json';
     a.click();
 }
+// Add a button on the map
+var exportButton = L.control({position: 'topright'});
+exportButton.onAdd = function(map) {
+    var div = L.DomUtil.create('div', 'export-button');
+    div.innerHTML = '<button onclick="exportData()">Export Responses</button>';
+    return div;
+};
+exportButton.addTo(map);
+
+// Function to download JSON of all classified sections
+function exportData() {
+    var participantCode = prompt("Enter your expert code (e.g., EXP_01) for this download:");
+    
+    if (!participantCode) return;
+
+    // Add participant code to each feature
+    var exportGeoJSON = JSON.parse(JSON.stringify(secoesAMP)); // copy to avoid modifying map
+    exportGeoJSON.features.forEach(f => {
+        f.properties.participant_id = participantCode;
+    });
+
+    var blob = new Blob([JSON.stringify(exportGeoJSON, null, 2)], {type: "application/json"});
+    var url = URL.createObjectURL(blob);
+    var a = document.createElement('a');
+    a.href = url;
+    a.download = 'gentrification_classification.json';
+    a.click();
+}
 
 
